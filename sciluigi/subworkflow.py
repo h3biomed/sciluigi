5c9ddb5d5f78ef74841bcf5d5e5171b2cc2f3d33
Michael Soltow <michael_soltow@h3biomedicine.com>
2016-01-26 14:01:12 -0500

    Altering SubWorkflow task to not automatically create output methods

--- sciluigi/subworkflow.py
+++ sciluigi/subworkflow.py
@@ -4,14 +4,6 @@ log = logging.getLogger('sciluigi-interface')
 
 class SubWorkflowTask(sciluigi.Task):
 
-    def __init__(self, *args, **kwargs):
-        super(SubWorkflowTask, self).__init__(*args, **kwargs)
-        last_task = self.sub_workflow()
-        for attrname in dir(last_task):
-            attrval = getattr(last_task, attrname)
-            if attrname[0:4] == 'out_':
-                setattr(self, attrname, attrval)
-
     def new_task(self, instance_name, cls, **kwargs):
         instance_name = '%s - %s' % (self.instance_name, instance_name)
         while self.workflow_task._tasks.has_key(instance_name):
@@ -20,3 +12,7 @@ class SubWorkflowTask(sciluigi.Task):
 
     def sub_workflow(self):
         raise NotImplementedError
+        
+    def requires(self):
+        yield self.sub_workflow()
+        yield super(SubWorkflowTask, self).requires()
