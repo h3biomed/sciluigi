9516f9ae45669efc2d26f5ff70a9a3ac4a0d59a9
Michael Soltow <michael_soltow@h3biomedicine.com>
2016-01-27 13:12:50 -0500

    Adding task method to access all TargetInfo outputs

--- sciluigi/dependencies.py
+++ sciluigi/dependencies.py
@@ -98,36 +98,43 @@ class DependencyHelpers(object):
         '''
         return self._output_targets()
 
+    def all_outputs(self):
+        '''For use in sub-workflows if you want to extract all output TargetInfo objects from this task'''
+        return self._output_infos()
+
     def _output_targets(self):
         '''
         Extract output targets from the TargetInfo objects
         or functions returning those (or lists of both the earlier)
         for use in luigi's output() method.
         '''
-        output_targets = []
+        return [info.target for info in self._output_infos()]
+
+    def _output_infos(self):
+        output_infos = []
         for attrname in dir(self):
             attrval = getattr(self, attrname)
             if attrname[0:4] == 'out_':
-                output_targets = self._parse_outputitem(attrval, output_targets)
+                output_infos = self._parse_outputitem(attrval, output_infos)
 
-        return output_targets
+        return output_infos
 
-    def _parse_outputitem(self, val, targets):
+    def _parse_outputitem(self, val, target_infos):
         '''
         Recursively loop through lists of TargetInfos, or
         callables returning TargetInfos, or lists of ...
-        (repeat recursively) ... and return all targets.
+        (repeat recursively) ... and return all TargetInfos.
         '''
         if callable(val):
             val = val()
         if isinstance(val, TargetInfo):
-            targets.append(val.target)
+            target_infos.append(val)
         elif isinstance(val, list):
             for valitem in val:
-                targets = self._parse_outputitem(valitem, targets)
+                target_infos = self._parse_outputitem(valitem, target_infos)
         elif isinstance(val, dict):
             for _, valitem in iteritems(val):
-                targets = self._parse_outputitem(valitem, targets)
+                target_infos = self._parse_outputitem(valitem, target_infos)
         else:
             raise Exception('Input item is neither callable, TargetInfo, nor list: %s' % val)
-        return targets
+        return target_infos
