419c2ea1fa8b45a142f2c0a8044bf09410825ea2
Michael Soltow <michael_soltow@h3biomedicine.com>
2016-02-04 13:44:50 -0500

    Trying to fix the case where a subworkflow does not get run if nothing requires it

--- sciluigi/subworkflow.py
+++ sciluigi/subworkflow.py
@@ -1,4 +1,5 @@
 import logging
+import luigi
 import sciluigi
 
 log = logging.getLogger('sciluigi-interface')
@@ -8,7 +9,7 @@ class SubWorkflowTask(sciluigi.task.Task):
 
     def __init__(self, *args, **kwargs):
         super(SubWorkflowTask, self).__init__(*args, **kwargs)
-        self.sub_workflow()
+        #self.sub_workflow()
 
     def new_task(self, instance_name, cls, **kwargs):
         instance_name = '%s - %s' % (self.instance_name, instance_name)
@@ -18,3 +19,11 @@ class SubWorkflowTask(sciluigi.task.Task):
 
     def sub_workflow(self):
         raise NotImplementedError
+
+    def requires(self):
+        requirements = [super(SubWorkflowTask, self).requires()]
+        outputs = luigi.task.flatten(self.output())
+        for output in outputs:
+            requirements.append(output.task)
+        return requirements
+    
