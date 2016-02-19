2c7f28703ddd1f212aac3976af8b15668bba1409
Michael Soltow <michael_soltow@h3biomedicine.com>
2016-02-19 13:39:44 -0500

    Removing the dupe task names and passing that responsibility onto the user

--- sciluigi/subworkflow.py
+++ sciluigi/subworkflow.py
@@ -13,8 +13,6 @@ class SubWorkflowTask(sciluigi.task.Task):
 
     def new_task(self, instance_name, cls, **kwargs):
         instance_name = '%s - %s' % (self.instance_name, instance_name)
-        while self.workflow_task._tasks.has_key(instance_name):
-            instance_name += ' - Dupe'
         return self.workflow_task.new_task(instance_name, cls, **kwargs)
 
     def sub_workflow(self):
