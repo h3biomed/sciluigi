e8931507eab568fbebde468b8c43295ec8100e49
Michael Soltow <michael_soltow@h3biomedicine.com>
2016-03-30 13:12:29 -0400

    Trying once again to not run the sub_workflow() method on construction

--- sciluigi/subworkflow.py
+++ sciluigi/subworkflow.py
@@ -7,10 +7,6 @@ log = logging.getLogger('sciluigi-interface')
 
 class SubWorkflowTask(sciluigi.task.Task):
 
-    def __init__(self, *args, **kwargs):
-        super(SubWorkflowTask, self).__init__(*args, **kwargs)
-        self.sub_workflow()
-
     def new_task(self, instance_name, cls, **kwargs):
         instance_name = '%s - %s' % (self.instance_name, instance_name)
         return self.workflow_task.new_task(instance_name, cls, **kwargs)
@@ -19,6 +15,7 @@ class SubWorkflowTask(sciluigi.task.Task):
         raise NotImplementedError
 
     def requires(self):
+        self.sub_workflow()
         requirements = [super(SubWorkflowTask, self).requires()]
         for output in self.output_infos():
             requirements.append(output.task)
