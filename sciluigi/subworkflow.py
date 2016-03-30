d5bd8e1926f0fc5a496eed973d397164c17d904a
Michael Soltow <michael_soltow@h3biomedicine.com>
2016-03-30 17:36:09 -0400

    Fixing an inheritance bug

--- sciluigi/subworkflow.py
+++ sciluigi/subworkflow.py
@@ -1,5 +1,4 @@
 import logging
-import luigi
 import sciluigi
 
 log = logging.getLogger('sciluigi-interface')
@@ -7,17 +6,17 @@ log = logging.getLogger('sciluigi-interface')
 
 class SubWorkflowTask(sciluigi.task.Task):
 
-    def new_task(self, instance_name, cls, **kwargs):
-        instance_name = '%s - %s' % (self.instance_name, instance_name)
-        return self.workflow_task.new_task(instance_name, cls, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super(sciluigi.task.Task, self).__init__(*args, **kwargs)
+        self.initialize_tasks()
+        self.initialize_inputs_and_outputs()
 
     def initialize_tasks(self):
         raise NotImplementedError
 
-    def initialize_inputs_and_outputs(self):
-        # Sneaks initialize_tasks() into the Task constructor before initialize_inputs_and_outputs gets called
-        self.initialize_tasks()
-        super(SubWorkflowTask, self).initialize_inputs_and_outputs()
+    def new_task(self, instance_name, cls, **kwargs):
+        instance_name = '%s - %s' % (self.instance_name, instance_name)
+        return self.workflow_task.new_task(instance_name, cls, **kwargs)
 
     def connect_tasks(self):
         raise NotImplementedError
