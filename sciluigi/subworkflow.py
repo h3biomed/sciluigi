802255020a48bf934a09c4a8ebda0fc45f58fd0b
Michael Soltow <michael_soltow@h3biomedicine.com>
2016-04-05 12:01:51 -0400

    More sciluigi debugging

--- sciluigi/subworkflow.py
+++ sciluigi/subworkflow.py
@@ -8,8 +8,11 @@ class SubWorkflowTask(sciluigi.task.Task):
 
     def __init__(self, *args, **kwargs):
         super(sciluigi.task.Task, self).__init__(*args, **kwargs)
+        log.info('Initializing tasks for ' + self.__class__.__name__)
         self.initialize_tasks()
+        log.info('Initializing inputs and outputs for ' + self.__class__.__name__)
         self.initialize_inputs_and_outputs()
+        log.info('Connecting tasks for ' + self.__class__.__name__)
         self.endpoints = [self.connect_tasks()]
 
     def initialize_tasks(self):
