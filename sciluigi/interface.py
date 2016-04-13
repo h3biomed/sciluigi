02c80691ede3120388e7f8e6be9c015607291eab
Michael Soltow <michael_soltow@h3biomedicine.com>
2016-04-13 12:52:48 -0400

    Changing logging to remove log files

--- sciluigi/interface.py
+++ sciluigi/interface.py
@@ -24,29 +24,22 @@ def setup_logging():
     sciluigi_log_formatter = logging.Formatter(LOGFMT_SCILUIGI, DATEFMT)
 
     # Stream handler (for STDERR)
-    stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(stream_formatter)
-    stream_handler.setLevel(logging.INFO)
+    luigi_stream_handler = logging.StreamHandler()
+    luigi_stream_handler.setFormatter(luigi_log_formatter)
+    luigi_stream_handler.setLevel(logging.INFO)
 
-    # File handler
-    luigi_file_handler = logging.FileHandler(log_path)
-    luigi_file_handler.setFormatter(luigi_log_formatter)
-    luigi_file_handler.setLevel(logging.DEBUG)
-
-    sciluigi_file_handler = logging.FileHandler(log_path)
-    sciluigi_file_handler.setFormatter(sciluigi_log_formatter)
-    sciluigi_file_handler.setLevel(logging.DEBUG)
+    sciluigi_stream_handler = logging.StreamHandler()
+    sciluigi_stream_handler.setFormatter(sciluigi_log_formatter)
+    sciluigi_stream_handler.setLevel(logging.DEBUG)
 
     # Loggers
     luigi_logger = logging.getLogger('luigi-interface')
-    luigi_logger.addHandler(luigi_file_handler)
-    luigi_logger.addHandler(stream_handler)
-    luigi_logger.setLevel(logging.WARN)
+    luigi_logger.addHandler(luigi_stream_handler)
+    luigi_logger.setLevel(logging.DEBUG)
     luigi.interface.setup_interface_logging.has_run = True
 
     sciluigi_logger = logging.getLogger('sciluigi-interface')
-    sciluigi_logger.addHandler(stream_handler)
-    sciluigi_logger.addHandler(sciluigi_file_handler)
+    sciluigi_logger.addHandler(sciluigi_stream_handler)
     sciluigi_logger.setLevel(logging.DEBUG)
 
 setup_logging()
