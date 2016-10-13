6272d58d357e84f387dde3c075b3dc12e17fef71
Michael Soltow <michael_soltow@h3biomedicine.com>
2016-10-12 22:11:53 -0400

    Debugging

--- sciluigi/dependencies.py
+++ sciluigi/dependencies.py
@@ -217,6 +217,8 @@ class DependencyHelpers(object):
         '''
         Implement luigi API method
         '''
+        if hasattr(self, 'instance_name'):
+            log.info(self.instance_name + ' - ' + self._output_targets())
         return self._output_targets()
 
     def output_infos(self):
