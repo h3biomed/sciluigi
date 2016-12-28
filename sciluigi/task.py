4a0eab5a4a7e999f31e60c9b182340370156fc4d
Michael Soltow <michael_soltow@h3biomedicine.com>
2016-12-28 15:28:08 -0500

    Altering debugging

--- sciluigi/task.py
+++ sciluigi/task.py
@@ -25,8 +25,6 @@ def new_task(name, cls, workflow_task, **kwargs):
     kwargs['instance_name'] = name
     kwargs['workflow_task'] = workflow_task
     print name + ' - ' + cls.__name__
-    if 'sciluigi_reduce_function' in kwargs:
-        print kwargs
     newtask = cls(**kwargs)
     if slurminfo is not None:
         newtask.slurminfo = slurminfo
