d044c37df041b6a56fd519674e3a78da5c2048a7
Michael Soltow <michael_soltow@h3biomedicine.com>
2017-01-12 15:33:14 -0500

    Making sure subworkflow output is correctly written

--- sciluigi/workflow.py
+++ sciluigi/workflow.py
@@ -4,12 +4,13 @@ This module contains sciluigi's subclasses of luigi's Task class.
 
 import datetime
 import luigi
+from luigi.six import iteritems
 import logging
-import sciluigi
-import sciluigi.interface
-import sciluigi.dependencies
-import sciluigi.slurm
+import os
+import os.path as op
+import random
 from subprocess import check_call
+import time
 
 log = logging.getLogger('sciluigi-interface')
 
@@ -42,6 +43,15 @@ class WorkflowTask(luigi.Task):
         if self._wfstart == '':
             self._wfstart = datetime.datetime.utcnow().strftime('%Y%m%d_%H%M%S_%f')
 
+    def get_output_path(self):
+        '''
+        Get the path to the workflow-speicfic audit trail file.
+        '''
+        self._ensure_timestamp()
+        clsname = self.__class__.__name__.lower()
+        output_dirpath = 'sciluigi-output/workflow_%s_started_%s.out' % (clsname, self._wfstart)
+        return output_dirpath
+
     def workflow(self):
         '''
         SciLuigi API methoed. Implement your workflow here, and return the last task(s)
@@ -71,7 +81,7 @@ class WorkflowTask(luigi.Task):
         '''
         Implementation of Luigi API method
         '''
-        return luigi.LocalTarget('sciluigi/_SUCCESS')
+        return luigi.LocalTarget(self.get_output_path())
 
     def run(self):
         '''
@@ -91,6 +101,37 @@ class WorkflowTask(luigi.Task):
             log.info('SciLuigi: %s Workflow Finished', clsname)
             log.info('-'*80)
             self._hasloggedfinish = True
+
+    @luigi.Task.event_handler(luigi.Event.PROCESSING_TIME)
+    def save_end_time(self, task_exectime_sec):
+        '''
+        Log end of execution of task, with execution time.
+        '''
+        msg = 'Workflow {task} finished after {proctime:.3f}s'.format(
+            task=self.get_instance_name(),
+            proctime=task_exectime_sec)
+        log.info(msg)
+        self._add_output_info(self.__class__.__name__.lower(), 'task_exectime_sec', '%.3f' % task_exectime_sec)
+        for paramname, paramval in iteritems(self.param_kwargs):
+            if paramname not in ['workflow_task']:
+                self.add_auditinfo(paramname, paramval)
+
+    def _add_output_info(self, instance_name, infotype, infoval):
+        '''
+        Save audit information in a designated file, specific for this task.
+        '''
+        dirpath = self.get_output_path()
+        if not op.isdir(dirpath):
+            time.sleep(random.random())
+            if not os.path.isdir(dirpath):
+                os.makedirs(dirpath)
+
+        output_file = os.path.join(dirpath, instance_name)
+        if not os.path.exists(output_file):
+            with open(output_file, 'w') as afile:
+                afile.write('[%s]\n' % self.instance_name)
+        with open(output_file, 'a') as afile:
+            afile.write('%s: %s\n' % (infotype, infoval))
 
 # ================================================================================
 
