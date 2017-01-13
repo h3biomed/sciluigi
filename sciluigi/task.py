27cb34669c8a0a0220b684798e22235b490c62f4
Michael Soltow <michael_soltow@h3biomedicine.com>
2017-01-13 10:07:53 -0500

    Renaming first arg so that it is less likely to clash with anything in kwargs

--- sciluigi/task.py
+++ sciluigi/task.py
@@ -15,12 +15,12 @@ log = logging.getLogger('sciluigi-interface')
 # ==============================================================================
 
 
-def new_task(name, cls, workflow_properties, **kwargs):
+def new_task(instance_name, cls, workflow_properties, **kwargs):
     slurminfo = None
     if 'sciluigi_reduce_function' not in kwargs:
-        kwargs['sciluigi_reduce_args'] = (name, cls, workflow_properties, copy.deepcopy(kwargs))
+        kwargs['sciluigi_reduce_args'] = (instance_name, cls, workflow_properties, copy.deepcopy(kwargs))
         kwargs['sciluigi_reduce_function'] = _new_task_unpickle
-    kwargs['instance_name'] = name
+    kwargs['instance_name'] = instance_name
     kwargs['workflow_properties'] = workflow_properties
     newtask = cls(**kwargs)
     if slurminfo is not None:
