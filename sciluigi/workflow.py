a0e0c1c57b24614cf1766337ec1c99d6543677a6
lebolo <luis.lebolo@tessella.com>
2017-06-23 18:40:13 +0000

    Add ability to mirror_output with an identifier

--- sciluigi/workflow.py
+++ sciluigi/workflow.py
@@ -23,15 +23,18 @@ class WorkflowTask(sciluigi.task.Task):
     def requires(self):
         return [info.task for info in self.output_infos()]
 
-    def mirror_outputs(self, inner_workflow):
+    def mirror_outputs(self, inner_workflow, element_id=None):
+        # if isinstance(attrval, Mapping):
+        #     for key in attrval:
+        #         getattr(self, attrname)[key].receive_from(getattr(inner_workflow, attrname)[key])
+        # elif isinstance(attrval, Sequence):
+        #     for item in attrval:
+        #         get
+        # getattr(self, attrname).receive_from(getattr(inner_workflow, attrname))
+
         for attrname, attrval in iteritems(inner_workflow.__dict__):
             if 'out_' == attrname[0:4]:
-                setattr(self, attrname + '-' + inner_workflow.instance_name, attrval)
-
-                # if isinstance(attrval, Mapping):
-                #     for key in attrval:
-                #         getattr(self, attrname)[key].receive_from(getattr(inner_workflow, attrname)[key])
-                # elif isinstance(attrval, Sequence):
-                #     for item in attrval:
-                #         get
-                # getattr(self, attrname).receive_from(getattr(inner_workflow, attrname))
+                if element_id is not None:
+                    setattr(self, '%s-%s-%s' % (attrname, element_id, inner_workflow.instance_name), attrval)
+                else:
+                    setattr(self, '%s-%s' % (attrname, inner_workflow.instance_name), attrval)
